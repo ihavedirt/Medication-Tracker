b355b1eb9fd99b317893d34ecd0b35d8b353320e
Rhet Bernard <rhb607@usask.ca>
2024-11-26 16:19:02 -0600

    Resolved error importing supabase as ../server to ../client

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,13 +1,42 @@
-import { createClient } from '../../utils/supabase/server';
+"use client";
+import { createClient } from '../../utils/supabase/client';
+import { useState, useEffect } from "react"
 import Weekview from '../ui/weekview';
 
 
-export default async function Notes() {
+export default async function Dashboard() {
     const supabase = createClient();
     const { data: notes } = await supabase.from("notes").select();
 
+    /*const [subprofiles, setSubprofiles] = useState([]);
+    const [medications, setMedications] = useState([]);
+
+    // Fetching primary and sub user profile data
+    useEffect(() => {
+        const fetchSessionAndSubprofiles = async () => {
+            const { data: session } = await supabase.auth.getUser();
+            setMySession(session);
+            
+            if (session) {
+                const { data: subprofileData, error } = await supabase
+                    .from('subprofiles') 
+                    .select('id, first_name, last_name')
+                    .eq('uuid', session.user.id);
+
+                if (error) {
+                    console.error("Error fetching subprofiles:", error);
+                } else {
+                    setSubprofiles(subprofileData);
+                }
+            }
+        };
+
+        fetchSessionAndSubprofiles();
+    }, []);*/
+
+
     return <div><Weekview
-    subusers_fname={subprofiles.first_name}
-    subusers_lname={subprofiles.last_name}
+    /*subusers_fname={subprofiles.first_name}
+    subusers_lname={subprofiles.last_name}*/
     /></div>
 }
