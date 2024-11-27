e17bab83dbdbd2630ad0a2fc5777195af11c8d75
David Baik <davidbaik@Davids-MacBook-Pro.local>
2024-11-27 10:01:15 -0600

    got rid of error messages. DB

--- app/api/twilio/route.js
+++ app/api/twilio/route.js
@@ -1,4 +1,5 @@
 import twilio from 'twilio';
+import { NextResponse } from 'next/server'
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -7,23 +8,17 @@ const client = require('twilio')(accountSid, authToken);
 // https://www.twilio.com/docs/messaging/tutorials/how-to-send-sms-messages/node
 // ^ this has info if we need to modify to send to multiple people
 // keep this async if we gonna access the backend here
-export async function POST(req) {
+export async function POST(req, res) {
     //if (req.method === 'POST') {
         // this needs to be integrated
         // const { number, body } = JSON.parse(req.body);
-    try{
-        client.messages
-            .create({
-            body: 'new message 2',
-            from: '+16812400904',
-            to: '+13062620223'
-        })
-        return NextResponse.json({ status: 200 });
-    } 
-    catch (error) {
-        return NextResponse.json({ status: 500 });
-
-    }
+    client.messages
+        .create({
+        body: 'new message 2',
+        from: '+16812400904',
+        to: '+13062620223'
+    })
+    return NextResponse.json({ status: 200 })
 /*
         .then((message) => { // ok
             res.status(200).json({ messageSId: message.sid });
