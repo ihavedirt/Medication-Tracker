b27ec7ff6f82fa46c47036cbacc741b92042c400
David Baik <davidbaik@Davids-MBP.lan>
2024-11-27 00:58:45 -0600

    got api req handler and twilio messaging working, still errors tho. DB

--- app/api/twilio/route.js
+++ app/api/twilio/route.js
@@ -1,23 +1,35 @@
-import {NextRequest, NextResponse} from "next/server";
+import twilio from 'twilio';
 
-export async function GET (request){
-    const greeting = "Hello World!!"
-    const json = {
-        greeting
-    };
-
-    return NextResponse.json(json);
-}
-
-const accountSid = 'ACadcdb09c6b02e545518ceab1492bf724';
-const authToken = '[AuthToken]';
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 
-client.messages
-    .create({
-        body: 'hello',
-        from: '+16812400904',
-        to: '+13062620223'
-    })
-    .then(message => console.log(message.sid))
-    .done();
+// https://www.twilio.com/docs/messaging/tutorials/how-to-send-sms-messages/node
+// ^ this has info if we need to modify to send to multiple people
+// keep this async if we gonna access the backend here
+export async function POST(req) {
+    //if (req.method === 'POST') {
+        // this needs to be integrated
+        // const { number, body } = JSON.parse(req.body);
+    try{
+        client.messages
+            .create({
+            body: 'new message 2',
+            from: '+16812400904',
+            to: '+13062620223'
+        })
+        return NextResponse.json({ status: 200 });
+    } 
+    catch (error) {
+        return NextResponse.json({ status: 500 });
+
+    }
+/*
+        .then((message) => { // ok
+            res.status(200).json({ messageSId: message.sid });
+        })
+        .catch((err) => { // bad :(
+            res.status(500).json({ err: err.message });
+        });*/
+    //}
+}
