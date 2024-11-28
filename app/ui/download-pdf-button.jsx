bbdbc86a178053c4fb460c814f3b78ec2e871c7c
Tyrell Beauchamp <trb962@usask.ca>
2024-11-28 13:42:52 -0600

    Filtering data currently. PDF Download working

--- app/ui/download-pdf-button.jsx
+++ app/ui/download-pdf-button.jsx
@@ -1,24 +1,53 @@
 import { jsPDF } from "jspdf";
 import Button from '@mui/material/Button'
-export default function DownloadPDFButton() {
+import { user } from "@nextui-org/theme";
+export default function DownloadPDFButton({userData, medicineData}) {
     const handleDownload = () => {
         const doc = new jsPDF();
 
+        const DosageUnit = useMemo(() => ({
+            mL: 0,
+            L: 1,
+            mg: 2,
+            g: 3,
+            kg: 4,
+        }), []);
+        
+        const MedType = useMemo(() => ({
+            Pill: 0,
+            Syrup: 1,
+            Injection: 2,
+        }), []);
+        
+        const Frequency = useMemo(() => ({
+            "Once Daily": 0,
+            "Twice Daily": 1,
+            "Three Daily": 2,
+            "Four Daily": 3,
+            "Every 6 Hours": 4,
+            "Every 8 Hours": 5,
+            "Every 12 Hours": 7,
+            "Weekly": 8,
+            "Monthly": 9,
+            "As Needed": 10,
+        }), []);
         // Add content to the PDF
         doc.setFontSize(18);
         doc.text('Medication Report', 105, 20, { align: 'center' });
 
-        const medications = [
-            { id: 1, name: 'Aspirin', dosage: '100mg' },
-            { id: 2, name: 'Paracetamol', dosage: '500mg' },
-        ];
-
         let y = 30;
         doc.setFontSize(12);
-        medications.forEach((med) => {
-            doc.text(`ID: ${med.id} | Name: ${med.name} | Dosage: ${med.dosage}`, 10, y);
-            y += 10;
+
+        userData.map((user) => {
+            medicineData.filter((medicine) => medicine.subprofile_id == user.id).forEach((med) => {
+                doc.text(`First Name: ${user.first_name} | Last Name: ${user.last_name} | Dosage: ${med.dosage}`, 10, y);
+                y += 10;
+            });
         });
+        /*medications.forEach((med) => {
+            doc.text(`First Name: ${med.first_name} | Last Name: ${med.name} | Dosage: ${med.dosage}`, 10, y);
+            y += 10;
+        });*/
 
         // Download the PDF
         doc.save('Medication_Report.pdf');
